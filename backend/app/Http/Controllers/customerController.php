3fe832f74750c70c36057543d8b14c8aa669f54a
ousama lasri <ousamalasri2@gmail.com>
2023-04-05 21:59:32 +0000

    first commit

---
+++ backend/app/Http/Controllers/customerController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Resources\customerCollection;
+use App\Http\Resources\customerResource;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use PhpParser\Node\Scalar\MagicConst\Method;
+
+class customerController extends Controller
+{
+    public function index(){
+        return new customerCollection(customer::all());
+    }
+
+    public function show(Customer $customer){
+        return new customerResource($customer);
+    }
+    public function store(StoreCustomerRequest $request){
+        Customer::create($request->validated());
+        return response()->json('created customer');
+    }
+    public function update(StoreCustomerRequest $request , Customer $customer){
+        $customer->update($request->validated());
+        return response()->json('costumer updated');
+    }
+    public function destroy( Customer $customer){
+        $customer->delete();
+        return response()->json('costumer deleted');
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\customerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::apiResource('customer',customerController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
