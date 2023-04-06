98e884999b681e3a62d47a80a5448f719f1e448d
ousama lasri <ousamalasri2@gmail.com>
2023-04-06 22:04:28 +0000

    create navbar

---
+++ frontend/fresh-fish-online/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import navbar from './components/navbar.vue'
+</script>
+
+<template>
+  <navbar />
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/fresh-fish-online/src/components/navbar.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="bg-black text-gray-100 py-6 px-6 shadow md:flex justify-between items-center">
+        <div class="flex item-center">
+            <span class="text-white text-xl mr-1">
+                fresh frish online
+            </span>
+
+        </div>
+        <div id="search-div" class="flex justify-center items-center">
+            <input id="search" class="h-10 rounded-2xl" type="text">
+            <div class="mx-5"><svg xmlns="http://www.w3.org/2000/svg" class="icon cursor-pointer icon-tabler icon-tabler-search" width="24"
+                    height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+                    <path d="M21 21l-6 -6"></path>
+                </svg></div>
+        </div>
+        <ul class="md:flex md:items-center">
+            <li class="md:mx-4">
+                <a class="text-xl bg-white rounded-2xl py-2 px-8 text-black transition-opacity hover:text-gray-300"
+                    href="">login</a>
+            </li>
+            <li class="">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                    class="icon icon-tabler icon-tabler-shopping-cart cursor-pointer hover:text-gray-500" width="24"
+                    height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M17 17h-11v-14h-2"></path>
+                    <path d="M6 5l14 1l-1 7h-13"></path>
+                </svg>
+            </li>
+        </ul>
+    </div>
+    <div class="bg-myblue text-gray-100 py-6 px-6 shadow md:flex justify-between items-center">
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#search{
+    width: 300px;
+}
+</style>
