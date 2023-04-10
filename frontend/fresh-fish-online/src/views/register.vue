95d55f60fee79ce64513c763111073617a54fe2b
ousama lasri <ousamalasri2@gmail.com>
2023-04-10 21:59:55 +0000

    modified login

--- frontend/fresh-fish-online/src/views/register.vue
+++ frontend/fresh-fish-online/src/views/register.vue
@@ -3,33 +3,33 @@
     <nav3 />
     <section class="register">
         <h2 class="font-extrabold text-4xl text-center mb-5 mt-24">Login</h2>
-        <a href class="py-2 w-1/2 mx-auto text-sm text-gray-600 hover:text-gray-950">Don't have an account? Register here.</a>
-        <form action="" class="relative">
-        <div class="flex flex-col items-center text-left justify-center mb-6">
+        
+        <form action="" class="flex mb-10 flex-col justify-center  items-center">
+            <a href class="py-5  w-1/2 mx-auto text-sm text-gray-600 hover:text-gray-950">Don't have an account? Register here.</a>
+            <div class="text-left justify-center mb-6">
             <div class="">
-                <label class="blo text-gray-600 font-bold text-right mb-10" for="email">
+                <label class="block text-gray-600 font-bold text-left mb-10 " for="email">
                     Email
                 </label>
             </div>
-            <div class="w-2/3">
+            <div class="w-[40rem]">
                 <input class="form-input block pl-10 rounded-md h-12 w-full focus:bg-white" id="my-textfield" type="text" value="">
             </div>
         </div>
-        <div class="flex flex-col items-center text-left justify-center mb-6">
+        <div class="flex flex-col  text-left justify-center mb-6">
             <div class="">
                 <label class="block text-gray-600 font-bold text-left mb-10 " for="email">
                     Password
                 </label>
             </div>
-            <div class="w-2/3">
+            <div class="w-[40rem]">
                 <input class="form-input block pl-10 rounded-md h-12 w-full focus:bg-white" id="my-textfield" type="text" value="">
             </div>
         </div>
         <div class="flex flex-col items-center text-left justify-center mb-6">
-            <div class="">
-                <button class="bg-myblue px-5 rounded-md text-white hover:bg-blue-900">clear cart</button>
+            <div class="w-[40rem]">
+                <button class="bg-myblue px-8  py-2 rounded-md text-white hover:bg-blue-900">Login</button>
             </div>
-            
         </div>
     </form>
 
